d93cdb1f2d8277327d06eb2b4f6b4e8390e5baf3
Mustafa ilahi <mustafailahi586@gmail.com>
2024-12-21 18:54:35 +0500

    updated

--- App.js
+++ App.js
@@ -1,28 +1,12 @@
-import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import {StyleSheet} from 'react-native';
 import React from 'react';
-import {colors} from './src/services';
-import Main from './src/screens/Main';
-import Login from './src/screens/Login';
-import ResetPassword from './src/screens/ResetPassword';
-import PasswordConfirmed from './src/screens/PasswordConfirmed';
 import MainNavigator from './src/services/config/navigation';
-import OTPVerification from './src/screens/OTPVerification';
-import ForgotPassword from './src/screens/ForgotPassword';
-import Signup from './src/screens/Signup';
-import ProfileOnboarding from './src/screens/ProfileOnboarding';
+
 
 export default function App() {
   return (
     <MainNavigator />
-
-    // <Main />
-    // <Login />
-    // <ResetPassword />
-    // <PasswordConfirmed />
-    // <OTPVerification />
-    // <ForgotPassword />
-    // <Signup />
-    // <ProfileOnboarding />
+    
   );
 }
 
